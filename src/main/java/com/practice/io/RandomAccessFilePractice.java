557ab18ba6870aa00d59708340630bd4d3780331
rohithraj143 <rohith.raj.gre@gmail.com>
2017-12-05 20:05:24 -0500

    new changes

--- src/main/java/com/practice/io/RandomAccessFilePractice.java
+++ src/main/java/com/practice/io/RandomAccessFilePractice.java
@@ -1,6 +1,7 @@
 package com.practice.io;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
 
@@ -9,11 +10,25 @@ public class RandomAccessFilePractice {
     public static void main(String[] args) {
 
         try {
-            RandomAccessFile file = new RandomAccessFile("", "rw");
-            
+            RandomAccessFile file = new RandomAccessFile("C:\\Code\\External_Resources\\input.txt", "rw");
+            RandomAccessFile output = new RandomAccessFile("C:\\Code\\External_Resources\\output.txt", "rw");
+            file.seek(100);
+            output.seek(90);
+
+            long filePointer = file.getFilePointer();
+
+            System.out.println("FilePractice pointer is " + filePointer);
+
+            int i = file.read();
+            while(i != -1) {
+
+                System.out.println((char) i);
+                output.write(i);
+                i = file.read();
+            }
 
 
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
